74e9c1c47b732afe335a29494e142543795cee1b
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-08-17 22:18:02 +0000

    COMMIT FOR task 1

---
+++ alx_book_store.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store
+
+USE alx_book_store
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL ,
+    price DOUBLE NOT NULL,
+    publication_date DATE NOT NULL,
+    FOREIGN KEY (author_id) KEY REFRENCE Authors(author_id) 
+)
+
+
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id PRIMARY KEY AUTO_INCREMENT,
+    customer_id int NOT NULL, 
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFRENCE Customers(customer_id)
+)
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid PRIMARY KEY AUTO_INCREMENT,
+    order_id INT
+    book_id INT NOT NULL
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFRENCE orders(order_id)
+    FOREIGN KEY (book_id) REFRENCES Books(book_id)
+)
